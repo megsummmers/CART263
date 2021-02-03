ba061b82893610dbb4e25b6271462c9dac28091a
megsummmers <70597858+megsummmers@users.noreply.github.com>
2021-02-03 10:50:18 -0500

    Activity 02: Started
    Created and put the explanation of the game wihtin the folder

---
+++ Activities/Activity02-slamina/js/script.js
@@ -0,0 +1,52 @@
+/**************************************************
+Template p5 project
+Pippin Barr
+
+Here is a description of this template p5 project.
+**************************************************/
+"use strict";
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  createCanvas(1000, 1000);
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+  background(0);
+}
+
+function mousePressed(){
+  background(255);
+  if (annyang) {
+    let commands = {
+      'ozzy ozzy ozzy': function(){
+        responsiveVoice.speak("Oi! Oi! Oi!");
+      },
+      'hello': function(){
+        responsiveVoice.speak("Hello there! :D");
+      },
+      'how are you': function(){
+        responsiveVoice.speak("I'm good, how are you?");
+      },
+      "I'm good": function(){
+        responsiveVoice.speak("That's great! :)");
+      },
+      "I'm bad": function(){
+        responsiveVoice.speak("Oh no! Feel better soon");
+      },
+      "goodbye": function(){
+        responsiveVoice.speak("Bye! Come back soon");
+      }
+    };
+    annyang.addCommands(commands);
+    annyang.start();
+  } else {
+    alert('This page requires speech recognition.');
+  }
+  //responsiveVoice.speak("hello hello", "French Female");
+}
