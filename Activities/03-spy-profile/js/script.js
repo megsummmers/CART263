fd458e9935aaf6c5b128fc72f23e66dc1b06c2de
megsummmers <70597858+megsummmers@users.noreply.github.com>
2021-02-10 11:10:03 -0500

    Started Spy profile generator + changed sausage dog file name

---
+++ Activities/03-spy-profile/js/script.js
@@ -0,0 +1,23 @@
+/**************************************************
+Template p5 project
+Pippin Barr
+Meg Summers
+
+User gives a name then the program will generate a
+spy profile for them with a password that they need
+to use to access their profile
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
