9c5b986ee7678012e8dc6c2b2caadf0be7c514da
megsummmers <70597858+megsummmers@users.noreply.github.com>
2021-02-16 21:33:10 -0500

    Cleaned activity 04 - Added extra functions for repetitve code - added loading screen - added webcam version

--- Activities/04-bubble-popper/js/script.js
+++ Activities/04-bubble-popper/js/script.js
@@ -9,14 +9,28 @@ around by tracking their hand with ml5.js Handpose
 **************************************************/
 "use strict";
 
+//state of program
+let state = 'loading';
 //webcam var
-let video = undefined;
-//handpose model
-let handpose = undefined;
+let video;
+// The name of our model
+let modelName = `Handpose`;
 // current set of predictions
 let predictions = [];
 //the bubbles
-let bubble = undefined;
+let bubble;
+//the pin
+let pin = {
+  tip: {
+    x: undefined,
+    y: undefined
+  },
+  head: {
+    x: undefined,
+    y: undefined,
+    size: 22
+  }
+};
 
 // setup()
 function setup() {
@@ -29,11 +43,10 @@ function setup() {
   handpose = ml5.handpose(video, {
     flipHorizontal: true
   }, function() {
-    //console.log(`Model loaded`);
+    state = 'gameplay';
   });
   //listen for predictions
   handpose.on('predict', function(results){
-    console.log(results);
     predictions = results;
   });
   //Ze bubble
@@ -42,58 +55,99 @@ function setup() {
     y: height,
     size: 100,
     vx: 0,
-    vy: -2
+    vy: -5
   };
 }
 
 // draw()
 function draw() {
-  background(0);
+  if (state === 'loading'){
+    loadScreen();
+  } else if (state === 'gameplay'){
+    gameplay();
+  }
+}
+
+function loadScreen(){
+  background(162, 210, 245);
+  push();
+  stroke(0);
+  textSize(25);
+  textAlign(CENTER);
+  text(`Loading ${modelName}...`, width/2, height/2);
+  textSize(22);
+  text("Once program starts use your index finger to pop the bubbles", width/2, height/2 + 35);
+  pop();
+}
+
+function gameplay(){
+  //background version
+  //background(0);
+
+  //video feed version
+  const flippedVideo = ml5.flipImage(video);
+  image(flippedVideo, 0, 0, width, height);
 
   //finds coords of index finger if hand is visible
   if (predictions.length > 0){
-    let hand = predictions[0];
-    let index = hand.annotations.indexFinger;
-    let tip = index[3];
-    let base = index[0];
-    let tipX = tip[0];
-    let tipY = tip[1];
-    let baseX = base[0];
-    let baseY = base[1];
-    push();
-    noFill();
-    stroke(255, 255, 255);
-    strokeWeight(2);
-    line(baseX, baseY, tipX, tipY);
-    pop();
-    //pin head
-    push();
-    noStroke();
-    fill(255, 0, 0);
-    ellipse(baseX, baseY, 20);
-    pop();
-
+    //updates pin location
+    updatePin(predictions[0]);
     //check for bubble pop
-    let d = dist(tipX, tipY, bubble.x, bubble.y);
-    if (d < bubble.size/2){
-      //change to function
-      bubble.x = random(width);
-      bubble.y = height + bubble.size/2;
+    let d = dist(pin.tip.x, pin.tip.y, bubble.x, bubble.y);
+    if (d < bubble.size/2) {
+      resetBubble();
     }
+    displayPin();
   }
 
-  //move bubble up the screen
+  //Bubble controls
+  moveBubble();
+  //resets bubble when it hits the top
+  if (bubble.y < 0 - bubble.size/2) {
+    resetBubble();
+  }
+  displayBubble();
+}
+
+function updatePin(prediction){
+  pin.tip.x = prediction.annotations.indexFinger[3][0];
+  pin.tip.y = prediction.annotations.indexFinger[3][1];
+  pin.head.x = prediction.annotations.indexFinger[0][0];
+  pin.head.y = prediction.annotations.indexFinger[0][1];
+}
+
+//resets bubble to the bottom of the screen
+function resetBubble(){
+  bubble.x = random(width);
+  bubble.y = height + bubble.size/2;
+}
+
+//moves bubble based on velocity
+function moveBubble(){
   bubble.x += bubble.vx;
   bubble.y += bubble.vy;
-  //reset bubble when it hits the top
-  if (bubble.y < -bubble.size/2) {
-    bubble.x = random(width);
-    bubble.y = height + bubble.size/2;
-  }
-  //display the bubble
+}
+
+//displays the bubble
+function displayBubble(){
   push();
   fill(0, 100, 200, 175);
-  stroke(255, 255, 255);
+  stroke(255, 255, 255, 175);
   ellipse(bubble.x, bubble.y, bubble.size);
+  pop();
+}
+//displays pin
+function displayPin(){
+  push();
+  noFill();
+  stroke(255, 255, 255);
+  strokeWeight(2);
+  line(pin.head.x, pin.head.y, pin.tip.x, pin.tip.y);
+  pop();
+  //pin head
+  push();
+  noStroke();
+  fill(255, 0, 0);
+  ellipse(pin.head.x, pin.head.y, pin.head.size);
   pop();
 }
