c0122cfc31925fa532da5d572a29a5dbdb1db5a5
megsummmers <70597858+megsummmers@users.noreply.github.com>
2021-02-21 11:08:56 -0500

    P1: started project + added description with goals

---
+++ Projects/Project1/js/script.js
@@ -0,0 +1,28 @@
+/**************************************************
+Template p5 project
+Pippin Barr
+Meg Summers
+
+A character creator program basde on the movie spider man into the spider verse.
+The program will let user's create their own version of spiderman
+as well as fill out all information such as their backstory,
+powers, name etc. The program will also include (limited)
+visual customization includeing creating their own spiderman suit.
+
+Technologies to be used
+- Web storage to store their character settings and
+  create multiple characters.
+- The program will use ml5 to control the customizer
+  giving the effect that the user is swiping through
+  a screen to change options
+**************************************************/
+
+// setup()
+function setup() {
+
+}
+
+// draw()
+function draw() {
+
+}
