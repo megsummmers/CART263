14b1742625d0e3ed18cb44244ce7b559b43ae1f4
megsummmers <70597858+megsummmers@users.noreply.github.com>
2021-03-10 23:01:59 -0500

    finished activity 05, code still needs to be improved

--- Activities/05-haiku-generator/js/script.js
+++ Activities/05-haiku-generator/js/script.js
@@ -21,3 +21,61 @@ let sevenSyllableLines = [
   "Say the things left unsaid",
   "The autumn stretches its legs"
 ];
+
+let line1 = random(fiveSyllableLines);
+let line2 = random(sevenSyllableLines);
+let line3 = random(fiveSyllableLines);
+
+let line1P = document.getElementById('line1');
+let line2P = document.getElementById('line2');
+let line3P = document.getElementById('line3');
+
+line1P.innerText = line1;
+line2P.innerText = line2;
+line3P.innerText = line3;
+
+line1P.addEventListener('click', lineClick);
+line2P.addEventListener('click', lineClick);
+line3P.addEventListener('click', lineClick);
+
+function lineClick(event){
+  fadeOut(event.target, 1);
+}
+
+function setNewLine(element){
+  if (element === line1P || element === line3P){
+    element.innerText = random(fiveSyllableLines);
+  } else if (element === line2P){
+    element.innerText = random(sevenSyllableLines);
+  }
+}
+
+function random(array){
+  //Math.floor gets rid of any decimal numbers
+  //math.random gives a random number
+  let index = Math.floor(Math.random() * array.length);
+  return array[index];
+}
+
+function fadeOut(element, opacity){
+  opacity -= 0.02;
+  element.style['opacity'] = opacity;
+  if (opacity > 0){
+    requestAnimationFrame(function(){
+      fadeOut(element, opacity);
+    })
+  } else {
+    setNewLine(element);
+    fadeIn(element, opacity);
+  }
+}
+
+function fadeIn(element, opacity){
+  opacity += 0.02;
+  element.style['opacity'] = opacity;
+  if (opacity < 1){
+    requestAnimationFrame(function(){
+      fadeIn(element, opacity);
+    })
+  }
+}
