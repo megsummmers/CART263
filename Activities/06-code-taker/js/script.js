9a6ada9248c454248dcc1aae6ac56c580d4c5ef0
megsummmers <70597858+megsummmers@users.noreply.github.com>
2021-03-15 00:28:01 -0400

    finished activity 06

--- Activities/06-code-taker/js/script.js
+++ Activities/06-code-taker/js/script.js
@@ -5,7 +5,34 @@ Author Name
 This is a template. You must fill in the title,
 author, and this description to match your project!
 */
-
 "use strict";
 
+$(`.secret`).one(`mouseover`, function(event) {
+  $(this).addClass(`found`, 500);
+  $(this).draggable({
+    helper: `clone`
+  });
+});
+
+$(`#answer`).droppable({
+  drop: function(event, ui){
+    let letter = ui.draggable.text();
+    $(this).append(letter);
+    ui.draggable.draggable(`disable`);
+    ui.draggable.removeClass(`found`);
+    //check if right
+    if ($(this).text() === `Theremin`){
+      $(`#solved`).dialog(`open`);
+    }
+  }
+});
+
+$(`#solved`).dialog({
+  autoOpen: false,
+  buttons: {
+    "Why thank you": function(){
+      $(this).dialog(`close`);
+    }
+  }
+});
 // Code goes here
