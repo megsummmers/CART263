fe250694a4a3fc9bef859c395e86e3c6ef11f61f
megsummmers <70597858+megsummmers@users.noreply.github.com>
2021-03-16 21:53:27 -0400

    Cleaned up activity 06
    cleaned up and improved the code before duplicating it

--- Activities/06-raving-redactionist/js/script.js
+++ Activities/06-raving-redactionist/js/script.js
@@ -8,4 +8,31 @@ author, and this description to match your project!
 
 "use strict";
 
-// Code goes here
+let $secrets;
+
+setup();
+
+function setup() {
+  //saves all secrets to var and adds click listener
+  $secrets = $(`.secret`);
+  $secrets.on(`click`, redact);
+  //reveals secrets every 500 milliseconds
+  setInterval(revelation, 500);
+}
+
+function revelation(){
+  $(`.redacted`).each(attemptReveal);
+}
+
+function attemptReveal(){
+  let r = Math.random();
+  if (r < 0.1) {
+    $(this).removeClass(`redacted`);
+    $(this).addClass(`revealed`);
+  }
+}
+
+function redact(){
+  $(this).removeClass(`revealed`);
+  $(this).addClass(`redacted`);
+}
