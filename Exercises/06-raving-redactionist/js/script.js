c4319b760ce81f59b8ebb24f74cb60b5d52e7a35
megsummmers <70597858+megsummmers@users.noreply.github.com>
2021-03-18 12:38:53 -0400

    06: added timer and fixed up
    - Added timer to give the game an ending
    - Added title screen to choose difficulty
    - Added ending screen for win and lose

--- Exercises/06-raving-redactionist/js/script.js
+++ Exercises/06-raving-redactionist/js/script.js
@@ -1,4 +1,4 @@
-/**
+/***************************************************************
 Raving Redactionist
 Meg Summers
 
@@ -9,15 +9,21 @@ for a few minutes on one of 3 difficulties (num of paragraphs)
 
 Things to add:
 - 3 difficulties (done)
-- Timer for the user saving
+- Timer for ending
 - Title screen + ending screen
 - *Add animation to text
-*/
+- update look of website (done)
+****************************************************************/
 
 "use strict";
 
 let $secrets;
 let difficulty = "none";
+let timerMax = 30;
+let timerMin = 0;
+let timePassed = timerMax;
+let numofRedacted = 4;
+//constant
 
 //setup();
 
@@ -28,6 +34,29 @@ function setup() {
   //reveals secrets every 500 milliseconds
   if(difficulty != "none"){
     setInterval(revelation, 500);
+    setInterval(function(){
+      //get timer and redacted elements
+      let timerDisplay = document.getElementById('timer');
+      let $redacted = $(`.redacted`);
+      console.log($(`.redacted`).length);
+      //removes one from timer every second
+      timePassed -= 1;
+      if ($redacted.length === 0){
+        //end countdown
+        clearInterval();
+        //hide gameplay and show endscreen
+        ending('lose');
+      } else if (timePassed >= 10){ //displays the current text
+        timerDisplay.innerText = `0:${timePassed}`;
+      } else if (timePassed >= 0 && timePassed <= 9){
+        timerDisplay.innerText = `0:0${timePassed}`;
+      } else if (timePassed <= 0){
+        //end countdown
+        clearInterval();
+        //go to end screen
+        ending('win');
+      }
+    }, 1000);
   }
 }
 
@@ -62,12 +91,33 @@ function difficultySelect(choice){
   if (difficulty === "easy"){
     medium.style.display = "none";
     hard.style.display = "none";
+    numofRedacted = 4;
   } else if (difficulty === "medium"){
     hard.style.display = "none";
+    numofRedacted = 8;
+  } else if (difficulty === "hard"){
+    numofRedacted = 12;
   }
   //hide menu and show gameplay div
   gameplay.style.display = "inline";
   menu.style.display = "none";
   //start up the revealing
   setup();
+}
+
+function ending(results){
+  //hide gameplay and show endscreen
+  let gameplay = document.getElementById('gameplay');
+  let win = document.getElementById('win');
+  let lose = document.getElementById('lose');
+  //change the div
+  if (results === 'win'){
+    gameplay.style.display = "none";
+    win.style.display = "inline";
+    lose.style.display = "none";
+  } else if (results === 'lose'){
+    gameplay.style.display = "none";
+    win.style.display = "none";
+    lose.style.display = "inline";
+  }
 }
